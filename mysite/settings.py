1e9a4f095399e66bb35d34aaa6d1e1f952cf7ed8
Igor-Baryshnikov <jak9rrr@mail.ru>
2024-10-22 14:13:03 +0300

    security for mail

--- mysite/settings.py
+++ mysite/settings.py
@@ -142,8 +142,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 # Конфигурация сервера электронной почты
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'igorrus2005@gmail.com'
-EMAIL_HOST_PASSWORD = 'mqqpmvdvbplyriuq'
+EMAIL_HOST_USER = str(os.getenv('EMAIL_HOST_USER'))
+EMAIL_HOST_PASSWORD = str(os.getenv('EMAIL_HOST_PASSWORD'))
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
